438076de4507a6975cfac43b047396cbb469cf24
Jiaxin Zhao <jzhao537@uwo.ca>
2021-03-01 22:09:17 +0800

    commit input search

--- qqmusic/src/pages/search/components/search-input/index.js
+++ qqmusic/src/pages/search/components/search-input/index.js
@@ -3,12 +3,13 @@ import { SearchOutlined, CloseCircleOutlined } from '@ant-design/icons'
 import './style.css'
 class SearchInput extends Component {
     render() {
+        const { keyword, onKeywordChange, onClearKeyWord } = this.props
         return (
             <div className = "search-container">
                 <div className = "input-container">
                     <SearchOutlined style = {{color:'#999'}} />
-                    <input type = "text" className = "search-input" placeholder="请输入关键词" />
-                    <CloseCircleOutlined style={{color: '#999'}} />
+                    <input type = "text" value = {keyword} onChange = {onKeywordChange} className = "search-input" placeholder="请输入关键词" />
+                    {keyword !== '' ? <CloseCircleOutlined style={{color: '#999'}} onClick = {onClearKeyWord}/> : ''}
                 </div>
             </div>
         );
--- qqmusic/src/pages/search/index.js
+++ qqmusic/src/pages/search/index.js
@@ -3,16 +3,42 @@ import Header from '../../components/header'
 import SearchInput from './components/search-input'
 import { getSearchSuggest } from '../../api/music'
 class Search extends Component {
-  componentDidMount () {
-    getSearchSuggest('a').then(res => {
-      console.log(res)
+  constructor(props){
+    super(props)
+    this.state = {
+      keyword: '' // 搜索关键词
+    }
+    this.onKeywordChange = this.onKeywordChange.bind(this)
+    this.onClearKeyWord = this.onClearKeyWord.bind(this)
+  }
+  onKeywordChange (e) {
+    const keyword = e.target.value
+    this.setState({
+      ...this.state,
+      keyword
     })
   }
+  // 清空搜索框的内容
+  onClearKeyWord () {
+    this.setState({
+      ...this.state,
+      keyword: ''
+    })
+  }
+  componentDidMount () {
+    // getSearchSuggest('a').then(res => {
+    //   console.log(res)
+    // })
+  }
+
   render () {
+    const { keyword } = this.state
     return (
       <Fragment>
         <Header />
-        <SearchInput />
+        <SearchInput keyword = {keyword} 
+        onKeywordChange = {this.onKeywordChange}
+        onClearKeyWord = {this.onClearKeyWord}/>
       </Fragment>    
     )
   }
